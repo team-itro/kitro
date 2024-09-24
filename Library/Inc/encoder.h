f4468b8b585ace57013080b31dd7b2769521fd6f
Sundarbavan <sundarbavan2002@gmail.com>
2024-09-24 20:22:50 +0530

    encoder_modified

--- Library/Inc/encoder.h
+++ Library/Inc/encoder.h
@@ -6,7 +6,7 @@
 
 #define _ENCODER_START (u32)u32_max/1e4
 #define l_position TIM2-> CNT
-#define r_position (u32_max-TIM5-> CNT)
+#define r_position (u32_max-TIM3-> CNT)
 
 
 void encoderInit(void);
