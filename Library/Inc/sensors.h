9d2d4df61c1ad2f6fa8fd26f2dc33c66b0e1a4d3
thuvasooriya <thuvaragan28@gmail.com>
2024-09-26 21:25:21 +0530

    merging redundant files

--- Library/Inc/sensors.h
+++ Library/Inc/sensors.h
@@ -1,9 +1,9 @@
 #ifndef INC_SENSORS_H_
 #define INC_SENSORS_H_
 
+#include "CONSTANTS.h"
 #include "api.h"
-#include "main.h"
-#include <typedefs.h>
+// #include "main.h"
 
 float sharp_readv(adc_channels sharp_id);
 float sharp_readdist(adc_channels sharp_id);
