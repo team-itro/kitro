7f3c5cbe828a7e6c30f80a1eb0710593a2a623ed
thuvasooriya <thuvaragan28@gmail.com>
2024-09-26 21:06:06 +0530

    typedefs and constant definitions streamline

---
+++ Library/Inc/CONSTANTS.h
@@ -0,0 +1,100 @@
+#ifndef INC_CONSTANTS_H_
+#define INC_CONSTANTS_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define u32_max 4294967295
+
+#define u32 uint32_t
+#define u16 uint16_t
+#define u8 uint8_t
+
+#define SHARP_FR_CH ADC_CHANNEL_2
+#define SHARP_FL_CH ADC_CHANNEL_3
+#define SHARP_AR_CH ADC_CHANNEL_4
+#define SHARP_AL_CH ADC_CHANNEL_5
+
+#define REFLECTION_RATE_ 1000
+#define LOW_BAT_TH_ 7.4
+
+// ENCODER.h
+#define LINEAR_SENSITIVITY 67.7
+#define TURN_SENSITIVITY 5
+///////////////////////////// MOTOR.H //////////////////////////////////////////
+#define SPEED_THRESHOLD_ 0.8
+
+/////////////////////////////////// L3GD20.h
+////////////////////////////////////////////
+#define GYRO_SENSITIVITY_ 0.071
+#define NOISE_THRESHOLD_ .95
+
+//////////////////////////////////// DISPLAY.h
+///////////////////////////////////////////
+#define FPS 2 // MIN_ALLOWED --> .5
+
+/////////////////////////////////Buzzer.h /////////////////////////////////
+#define BUZZ_LOUDNESS_ .2
+
+typedef enum {
+  MOUSE_STATE_INIT_IDLE,
+  MOUSE_STATE_INIT_CONFIG,
+  MOUSE_STATE_INIT_RESET,
+  MOUSE_STATE_SEARCH_IDLE,
+  MOUSE_STATE_SEARCH_FORWARD,
+  MOUSE_STATE_SEARCH_BACK,
+  MOUSE_STATE_FAST_IDLE,
+  MOUSE_STATE_FAST_FORWARD,
+  MOUSE_STATE_FAST_BACK
+} MouseState;
+
+typedef enum {
+  DIR_F,
+  DIR_R,
+  DIR_B,
+  DIR_L,
+} DriveDirection;
+
+typedef struct {
+  MouseState current_state;
+  int16_t x;
+  int16_t y;
+  DriveDirection orientation;
+} Mouse;
+
+typedef void (*StateHandler)(void);
+
+typedef enum {
+  SHARP_FR,
+  SHARP_FL,
+  SHARP_AR,
+  SHARP_AL,
+  //	BAT_VOL
+} adc_channels;
+
+// typedef enum { WIN_TONE = 0, TONE1, TONE2, TONE3, TONE4 } BUZZ_Tones;
+
+typedef enum {
+  SMALL,  // 6x8
+  MEDIUM, // 7x10
+  LARGE,  // 11x18
+  HUGE    // 16x24
+} FONT_Size;
+
+typedef enum {
+  INIT = 0,
+  DEFAULT,
+  GYRO_CALIB,
+  SENSOR_READ,
+  LOW_BAT,
+  SUCESS_MSG,
+  SPEED_ADJ
+} DISP_State;
+
+// typedef enum { IDLE, STRAIGHT_RUN, POINT_TURN, FRONT_ALIGN } MV_Type;
+
+// typedef enum { SPEED1, SPEED2, SPEED3, ROT_SPEED } SPEED_TYPE;
+
+typedef enum { ONB } LEDS;
+
+#endif /* INC_CONSTANTS_H_ */
