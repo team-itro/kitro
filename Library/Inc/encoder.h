65dee1954139c54e4f8902762ffa8061c199c63a
Sundarbavan <sundarbavan2002@gmail.com>
2024-09-27 23:51:25 +0530

    encoder.h updated

--- Library/Inc/encoder.h
+++ Library/Inc/encoder.h
@@ -2,22 +2,13 @@
 #define INC_ENCODER_H_
 
 #include "CONSTANTS.h"
-#include "main.h"
+#include "api.h"
 
-// #define _ENCODER_START (u32) u32_max / 1e4
-// #define l_position TIM2->CNT
-// #define r_position (u32_max - TIM3->CNT)
-//
-// void encoderInit(void);
-// void encoderUpdate(void);
-// void resetEncoder(void);
-//
-// #define _ENCODER_START (u32) u32_max / 1e4
-// #define l_position TIM2->CNT
-// #define r_position (u32_max - TIM5->CNT)
-//
-// void encoderInit(void);
-// void encoderUpdate(void);
-// void resetEncoder(void);
-//
+ #define _ENCODER_START (u16) 1000 //(u32) u32_max / 1e4
+ #define l_position TIM3->CNT
+ #define r_position TIM2->CNT // (u32_max - TIM5->CNT)
+
+ void encoderInit(void);
+ void resetEncoder(void);
+
 #endif /* INC_ENCODER_H_ */
